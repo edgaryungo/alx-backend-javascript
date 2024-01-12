60a316ebb6ed999e4797a8edad0c3c33df01f493
Edgar <eddy8471@gmail.com>
2024-01-12 12:18:28 +0300

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,16 @@
-export default function getListStudentIds(arr) {
-    let newArr = [];
-    if (arr instanceof Array) {
-      newArr = arr.map((item) => item.id);
-    }
-  
-    return newArr;
-  }
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @author edgar
+* @returns
+*/
+export default function getListStudentIds(students) {
+ if (students instanceof Array) {
+   return students.map((student) => student.id);
+ }
+ return [];
+}
